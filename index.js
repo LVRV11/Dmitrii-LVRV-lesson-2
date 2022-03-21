c2215714f296c9362e1a11f2cd4b70682ad1e5ac
LVRV11 <lavrovdmitrii@gmail.com>
2022-03-21 09:44:15 +0100

    1q

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ let c = false;
 let test1 = !b.length === c; // False (b и c имеют разные типы данных)
 let test2 = a.length > b.length; // False (0 !> 3)
 let test3 = !a.length && !b.length; // True (оператор и)
-let test4 = a === c; // False (a и c имеют разные типы данных)
+let test4 = a === c; // False (a и c имеют разные типы данных)/
 
 
 
